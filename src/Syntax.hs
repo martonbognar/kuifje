fa865a054cb4e953c72cd5393bfa5b7eebc3efa2
marton bognar <martonbognar@sch.bme.hu>
2019-08-29 15:32:25 +0200

    Clean up Syntax.hs

--- src/Syntax.hs
+++ src/Syntax.hs
@@ -9,8 +9,10 @@ import Data.Semigroup
 
 import Distribution
 
+-- | Kleisli arrow.
 type a ~> b = a -> Dist b
 
+-- | Syntax of the Kuifje language.
 data Kuifje s
   = Skip
   | Update (s ~> s) (Kuifje s)
@@ -19,27 +21,32 @@ data Kuifje s
   | forall o. (Ord o) => Observe (s ~> o) (Kuifje s)
 
 instance Semigroup (Kuifje s) where
-  Skip         <> k  = k
-  Update f p   <> k  = Update f (p <> k)
-  While c p q  <> k  = While c p (q <> k)
-  If c p q r   <> k  = If c p q (r <> k)
-  Observe f p  <> k  = Observe f (p <> k)  -- added
+  Skip        <> k = k
+  Update f p  <> k = Update f (p <> k)
+  While c p q <> k = While c p (q <> k)
+  If c p q r  <> k = If c p q (r <> k)
+  Observe f p <> k = Observe f (p <> k)
 
 instance Monoid (Kuifje s) where
   mempty = Skip
   mappend = (<>)
 
+-- Return a 'skip' instruction.
 skip :: Kuifje s
 skip = Skip
 
+-- Return an 'update' instruction.
 update :: (s ~> s) -> Kuifje s
-update f  =  Update f skip
+update f = Update f skip
 
+-- Return a 'while' instruction.
 while :: (s ~> Bool) -> Kuifje s -> Kuifje s
-while c p  =  While c p skip
+while c p = While c p skip
 
+-- Return an 'if' instruction.
 cond :: (s ~> Bool) -> Kuifje s -> Kuifje s -> Kuifje s
-cond c p q  =  If c p q skip
+cond c p q = If c p q skip
 
+-- Return an 'observe' instruction.
 observe :: (Ord o) => (s ~> o) -> Kuifje s
 observe o = Observe o skip
