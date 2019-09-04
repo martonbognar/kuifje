f2c1b6668379197791785d2e5fd23082f3c7f9a1
marton bognar <martonbognar@sch.bme.hu>
2019-09-04 14:17:18 +0200

    Update some comments

--- src/Language/Kuifje/Distribution.hs
+++ src/Language/Kuifje/Distribution.hs
@@ -15,18 +15,20 @@ type Prob = Rational
 -- | Distribution data type.
 newtype Dist a = D { runD :: Map a Prob }
 
--- instance Hashable a => Hashable (Dist a) where
---   hashWithSalt salt (D s) = hashWithSalt salt s
-
+-- | Top-level fmap function for distributions.
 fmap :: (Ord b) => (a -> b) -> Dist a -> Dist b
 fmap f dx = dx >>= (return . f)
 
+-- | Top-level return function for distributions. Creates a singleton
+-- distribution.
 return :: (Ord a) => a -> Dist a
 return x = D $ singleton x 1
 
+-- | Top-level bind function for distributions.
 (>>=) :: (Ord b) => Dist a -> (a -> Dist b) -> Dist b
 d >>= f = D $ fromListWith (+) [(y, p * q) | (x, p) <- toList $ runD d, (y, q) <- toList $ runD (f x)]
 
+-- | Top-level join function for distributions.
 join :: (Ord a) => Dist (Dist a) -> Dist a
 join x = x >>= id
 
@@ -46,13 +48,13 @@ uniform l = D $ fromListWith (+) [(x, 1 / genericLength l) | x <- l]
 choose :: (Ord a) => Prob -> a -> a -> Dist a
 choose p x y = D $ fromListWith (+) [(x, p), (y, 1 - p)]
 
--- | Recover the list representation of a distribution, reduced.
+-- | Recover the map representation of a distribution, reduced.
 unpackD :: Dist a -> Map a Prob
 unpackD = removeZeroes . runD
   where
     removeZeroes = filter (/= 0)
 
--- | Remove duplicates and zeroes from a distribution.
+-- | Remove zeroes from a distribution.
 reduction :: Dist a -> Dist a
 reduction = D . unpackD
 
